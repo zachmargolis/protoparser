c58fc3e20ae414481dfc221045b6b84677f8ce28
Zach Margolis <margolis@squareup.com>
2014-07-08 20:11:34 -0700

    Undo builder refactor inside ProtoSchemaParser.java

--- src/main/java/com/squareup/protoparser/ProtoSchemaParser.java
+++ src/main/java/com/squareup/protoparser/ProtoSchemaParser.java
@@ -113,15 +113,8 @@ public final class ProtoSchemaParser {
     while (true) {
       String documentation = readDocumentation();
       if (pos == data.length) {
-        return new ProtoFile.Builder().setFileName(fileName)
-            .setPackageName(packageName)
-            .setDependencies(dependencies)
-            .setPublicDependencies(publicDependencies)
-            .setTypes(types)
-            .setServices(services)
-            .setOptions(options)
-            .setExtendDeclarations(extendDeclarations)
-            .build();
+        return new ProtoFile(fileName, packageName, dependencies, publicDependencies, types,
+            services, options, extendDeclarations);
       }
       Object declaration = readDeclaration(documentation, Context.FILE);
       if (declaration instanceof Type) {
@@ -239,14 +232,8 @@ public final class ProtoSchemaParser {
       }
     }
     prefix = previousPrefix;
-    return new MessageType.Builder().setName(name)
-        .setFullyQualifiedName(prefix + name)
-        .setDocumentation(documentation)
-        .setFields(fields)
-        .setNestedTypes(nestedTypes)
-        .setExtensions(extensions)
-        .setOptions(options)
-        .build();
+    return new MessageType(name, prefix + name, documentation, fields, nestedTypes, extensions,
+        options);
   }
 
   /** Reads an extend declaration. */
@@ -269,11 +256,7 @@ public final class ProtoSchemaParser {
     if (!name.contains(".") && packageName != null) {
       fqname = packageName + "." + name;
     }
-    return new ExtendDeclaration.Builder().setName(name)
-        .setFullyQualifiedName(fqname)
-        .setDocumentation(documentation)
-        .setFields(fields)
-        .build();
+    return new ExtendDeclaration(name, fqname, documentation, fields);
   }
 
   /** Reads a service declaration and returns it. */
@@ -295,12 +278,7 @@ public final class ProtoSchemaParser {
         options.add((Option) declared);
       }
     }
-    return new Service.Builder().setName(name)
-        .setFullyQualifiedName(prefix + name)
-        .setDocumentation(documentation)
-        .setOptions(options)
-        .setMethods(methods)
-        .build();
+    return new Service(name, prefix + name, documentation, options, methods);
   }
 
   /** Reads an enumerated type declaration and returns it. */
@@ -322,12 +300,7 @@ public final class ProtoSchemaParser {
         options.add((Option) declared);
       }
     }
-    return new EnumType.Builder().setName(name)
-        .setFullyQualifiedName(prefix + name)
-        .setDocumentation(documentation)
-        .setOptions(options)
-        .setValues(values)
-        .build();
+    return new EnumType(name, prefix + name, documentation, options, values);
   }
 
   /** Reads an field declaration and returns it. */
@@ -355,13 +328,7 @@ public final class ProtoSchemaParser {
       }
     }
     if (readChar() == ';') {
-      return new MessageType.Field.Builder().setLabel(labelEnum)
-          .setType(type)
-          .setName(name)
-          .setTag(tag)
-          .setDocumentation(documentation)
-          .setOptions(options)
-          .build();
+      return new MessageType.Field(labelEnum, type, name, tag, documentation, options);
     }
     throw unexpected("expected ';'");
   }
@@ -533,12 +500,7 @@ public final class ProtoSchemaParser {
       }
     } else if (readChar() != ';') throw unexpected("expected ';'");
 
-    return new Service.Method.Builder().setName(name)
-        .setDocumentation(documentation)
-        .setRequestType(requestType)
-        .setResponseType(responseType)
-        .setOptions(options)
-        .build();
+    return new Service.Method(name, documentation, requestType, responseType, options);
   }
 
   /** Reads a non-whitespace character and returns it. */
